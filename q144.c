d9082998e132b2b5c54b4773e68deb582fa4339e
UTKARSH-KAMBOJ <u.kamboj98@gmail.com>
2025-12-01 14:55:22 +0530

    Create q144.c

---
+++ q144.c
@@ -0,0 +1,34 @@
+Q144: Write a function that accepts a structure as parameter and prints its members.
+
+/*
+Sample Test Cases:
+Input 1:
+Student: Neha 104 92
+Output 1:
+Name: Neha | Roll: 104 | Marks: 92
+
+*/
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+// Function that accepts structure and prints its data
+void printStudent(struct Student s) {
+    printf("Name: %s | Roll: %d | Marks: %.0f\n", s.name, s.roll, s.marks);
+}
+
+int main() {
+    struct Student st;
+
+    printf("Enter Student Details (Name Roll Marks): ");
+    scanf("%s %d %f", st.name, &st.roll, &st.marks);
+
+    // Calling function
+    printStudent(st);
+
+    return 0;
+}
